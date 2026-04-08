863efd9be91c99974b2ee6b72b70c491fba30932
muhammed abdulsattar <122079443+albahagy@users.noreply.github.com>
2026-04-08 06:26:19 +0200

    Obscure API key in api_service.dart
    Obscured the API key for security reasons.

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   final _url = 'https://www.googleapis.com/books/v1/';
-  final _apiKey = 'AIzaSyAQH6e9eH75xcOpOPoV9aK4Km9tl68kT60';
+  final _apiKey = '############################';
   final Dio _dio;
   ApiService(this._dio);
   Future<Map<String, dynamic>> get({required String endPoint}) async {
